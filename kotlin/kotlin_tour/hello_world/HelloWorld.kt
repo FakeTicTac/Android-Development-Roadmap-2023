c696cf54e5e4bd191d983ddd2f539c4622524062
FakeTicTac <roman.makejev@gmail.com>
2023-11-03 23:47:04 +0200

    02: kotlin tour - hello world!

--- kotlin/kotlin_tour/hello_world/HelloWorld.kt
+++ kotlin/kotlin_tour/hello_world/HelloWorld.kt
@@ -1,5 +1,20 @@
 package kotlin_tour.hello_world
 
 fun main() {
+    // 1. Standard output functions
+    println("Hello, World!")
+    print("Hello, Kotlin!")
 
+    // 2. Variables and string templates
+    val popcornBoxes = 5
+
+    // ERROR: value is unchangeable
+    // popcornBoxes = 10
+
+    var popcornLovers = 10
+    println("There is $popcornLovers people that love popcorn.")
+
+    // Some people stopped loving popcorn.
+    popcornLovers = 5
+    println("There is only $popcornLovers people that love popcorn now!")
 }
---
+++ kotlin/kotlin_tour/hello_world/practice/Task01.kt
@@ -0,0 +1,8 @@
+package kotlin_tour.hello_world.practice
+
+fun main() {
+    val name = "Mary"
+    val age = 20
+
+    println("$name is $age years old")
+}

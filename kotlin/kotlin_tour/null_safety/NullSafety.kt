bf04777fc3c29434a784bd09890b03c8f27ed2c8
FakeTicTac <roman.makejev@gmail.com>
2023-11-05 23:47:50 +0200

    07: kotlin tour - null-safety.

--- kotlin/kotlin_tour/null_safety/NullSafety.kt
+++ kotlin/kotlin_tour/null_safety/NullSafety.kt
@@ -2,4 +2,29 @@ package kotlin_tour.null_safety
 
 fun main() {
 
+    // 01. Null value
+
+    var neverNull: String = "Hello, Kotlin!"
+    // neverNull = null
+
+    var canBeNull: String? = "Hello, Kotlin!"
+
+    var defaultNonNullable = "Hello, Kotlin"
+    // defaultNonNullable = null
+
+    // Check for null values
+    // canBeNull.length
+
+    // Safety check
+
+    if (canBeNull != null) {
+        println(canBeNull.length)
+    }
+
+    canBeNull = null
+    println(canBeNull?.length)
+
+    // Elvis operator (?:)
+    println(canBeNull?.length ?: "It's null :(")
+
 }
